c379ebde148fe48717a1fb29f04d9572df6470de
jhonrambo <karbunko1888@gmail.com>
2021-06-10 10:26:26 +0200

    reducido

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 
 class Tools():
@@ -35,27 +36,25 @@ class Board():
 
 class Jugador:
 
-    def __init__(self):
-        pass
-
-
-    def pedirJugadaJugador(self, token, board ):
+    def pedirJugadaJugador(self, token, board):
         try:
             choice = int(input(f"\n[{ token }] Elije del 1 - 9. > "))
-            # while not self.board.space_check(choice):
-            #    self.juega()
-            
-        except (ValueError, IndexError):
+        except (ValueError, IndexError, TypeError):
             print("\n       Numero invalido.")
             choice = self.pedirJugadaJugador(token, board)
-
-        if not board.space_check(choice):
-            print("\n Esa casilla ya esta usada")
+        if choice not in range(1, 10):
+            print("\n       Numero fuera de rango")
             choice = self.pedirJugadaJugador(token, board)
-
+        if not board.space_check(choice):
+            print("\n       Esa casilla ya esta usada")
+            choice = self.pedirJugadaJugador(token, board)
         return choice
 
+
 class Juego():
+
+    tokens = ['X', 'O']
+    posibilidades = [(1, 2, 3)] and [(4, 5, 6)] and [(7, 8, 9)] and [(1, 5, 9)] and [(3, 5, 7)] and [(1, 4, 7)] and [(3, 6, 9)]
 
     def __init__(self):
         Tools.clearScreen()
@@ -65,14 +64,24 @@ class Juego():
     def juega(self):
         self.board.refresh_screen()
         while True:
-            self.ejecutaTurno('X')
-            self.ejecutaTurno('O')
-
+            self.ejecutaTurno(self.tokens[0])
+            self.ejecutaTurno(self.tokens[1])
 
     def ejecutaTurno(self, token):
         choice = self.jugador.pedirJugadaJugador(token, self.board)
         self.board.update_cell(choice, token)
         self.board.refresh_screen()
+        self.comprobarGanador(token)
+
+    def comprobarGanador(self, token):
+        c = self.board.cells
+        for p in self.posibilidades:
+            jugada = [c[p[0]] + c[p[1]] + c[p[2]]]
+            jugada = c[1] + c[2] + c[3]
+            if jugada == token*3:
+                print(f"GANA {token}")
+                exit()
+
 
 juego = Juego()
 juego.juega()
